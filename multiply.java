ac657dee427b55186148190dabfcf07ef1d5e116
Nikhil Panjwani <nikhilpanjwani2001@gmail.com>
2023-01-08 20:15:32 +0000

    adding message multiply file

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+    
+        public static void main(String args[]){
+    
+            
+            System.out.println("Hello Nikhil");
+        }
+        }
+    
+    
+
